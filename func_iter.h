5aaa060b7880eeb40f4b4b744259746139fd12b1
Chase <44284917+TotallyNotChase@users.noreply.github.com>
2021-05-02 00:28:57 +0630

    Instance the iterator

--- func_iter.h
+++ func_iter.h
@@ -1,6 +1,12 @@
 ﻿#ifndef FUNC_ITER_H
 #define FUNC_ITER_H
 
+#include "maybe.h"
+#include "typeclass.h"
+#include "iterator.h"
 
+DefineMaybe(int, Int);
+
+DeclareIteratorOf(Int);
 
 #endif /* !FUNC_ITER_H */
