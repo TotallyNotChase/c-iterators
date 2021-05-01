b42d9bdbbda0b50f98df9d4d86d45fe99103ffd8
Chase <44284917+TotallyNotChase@users.noreply.github.com>
2021-05-01 23:55:24 +0630

    Initial commit

---
+++ func_iter.h
@@ -0,0 +1,6 @@
+﻿#ifndef FUNC_ITER_H
+#define FUNC_ITER_H
+
+
+
+#endif /* !FUNC_ITER_H */
---
+++ main.c
@@ -0,0 +1,9 @@
+﻿#include "func_iter.h"
+
+#include <stdio.h>
+
+int main(void)
+{
+    puts("haskell");
+    return 0;
+}
