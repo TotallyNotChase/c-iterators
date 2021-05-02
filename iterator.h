7395da8f27515134704c5402fde31a6f09787d50
Chase <44284917+TotallyNotChase@users.noreply.github.com>
2021-05-02 12:48:24 +0630

    Add macro for iterator typename + remove `static` from instance

--- iterator.h
+++ iterator.h
@@ -7,8 +7,10 @@
 #define DeclareIteratorOf(ElmntTypename)                                                                               \
     typeclass(Maybe(ElmntTypename) (*next)(void* self, void* ctx), ElmntTypename##Iterator)
 
+#define Iterator(ElmntTypename) ElmntTypename##Iterator
+
 #define instance_iterator(ItrType, ElmntTypename, CtxType, next_f, Name)                                               \
-    static ElmntTypename##Iterator Name(ItrType* x)                                                                    \
+    ElmntTypename##Iterator Name(ItrType* x)                                                                           \
     {                                                                                                                  \
         Maybe(ElmntTypename) (*const next_)(ItrType * self, CtxType * ctx) = (next_f);                                 \
         return (ElmntTypename##Iterator){.inst = {.next = (Maybe(ElmntTypename)(*)(void*, void*))next_}, .self = x};   \
