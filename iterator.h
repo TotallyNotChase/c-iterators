cca7cb2d02480bd5a3cc0028afb08ccc6225b917
Chase <44284917+TotallyNotChase@users.noreply.github.com>
2021-05-03 15:02:04 +0630

    Strictly typed context + non static iterator instance

--- iterator.h
+++ iterator.h
@@ -5,15 +5,16 @@
 #include "typeclass.h"
 
 #define DeclareIteratorOf(ElmntTypename)                                                                               \
-    typeclass(Maybe(ElmntTypename) (*next)(void* self, void* ctx), ElmntTypename##Iterator)
+    typeclass(Maybe(ElmntTypename) (*next)(void* self, void* ctx); void* ctx, ElmntTypename##Iterator)
 
 #define Iterator(ElmntTypename) ElmntTypename##Iterator
 
 #define instance_iterator(ItrType, ElmntTypename, CtxType, next_f, Name)                                               \
-    ElmntTypename##Iterator Name(ItrType* x)                                                                           \
+    ElmntTypename##Iterator Name(ItrType* x, CtxType* init_ctx)                                                        \
     {                                                                                                                  \
         Maybe(ElmntTypename) (*const next_)(ItrType * self, CtxType * ctx) = (next_f);                                 \
-        return (ElmntTypename##Iterator){.inst = {.next = (Maybe(ElmntTypename)(*)(void*, void*))next_}, .self = x};   \
+        return (ElmntTypename##Iterator){                                                                              \
+            .inst = {.next = (Maybe(ElmntTypename)(*)(void*, void*))next_, .ctx = init_ctx}, .self = x};               \
     }
 
 #endif /* !IT_ITERATOR_H */
