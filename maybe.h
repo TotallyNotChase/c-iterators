e71b97fffa256cf9ed0aebc76b2be7e62c38d044
Chase <44284917+TotallyNotChase@users.noreply.github.com>
2021-05-03 15:00:08 +0630

    Separate out anon union + More abstract utilities

--- maybe.h
+++ maybe.h
@@ -1,21 +1,27 @@
 #ifndef IT_MAYBE_H
 #define IT_MAYBE_H
 
+typedef enum
+{
+    MaybeTag_Nothing = 0,
+    MaybeTag_Some
+} MaybeTag;
+
 #define DefineMaybe(T, Typename)                                                                                       \
     typedef struct                                                                                                     \
     {                                                                                                                  \
-        enum                                                                                                           \
-        {                                                                                                              \
-            None = 0,                                                                                                  \
-            Some                                                                                                       \
-        } tag;                                                                                                         \
-        union                                                                                                          \
-        {                                                                                                              \
-            void* none;                                                                                                \
-            T val;                                                                                                     \
-        };                                                                                                             \
+        MaybeTag tag;                                                                                                  \
+        T val;                                                                                                         \
     } Maybe##Typename
 
 #define Maybe(Typename) Maybe##Typename
+
+#define Some(V, Typename)                                                                                              \
+    (Maybe(Typename)) { .tag = MaybeTag_Some, .val = (V) }
+
+#define Nothing(Typename)                                                                                              \
+    (Maybe(Typename)) { 0 }
+
+#define is_nothing(x) (x).tag == MaybeTag_Nothing
 
 #endif /* !IT_MAYBE_H */
