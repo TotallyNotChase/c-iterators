43eccd14aeb3060dcb6a58cdcfec3159df19a9c4
Chase <44284917+TotallyNotChase@users.noreply.github.com>
2021-05-04 21:16:52 +0630

    Use summation as an example for int array

--- main.c
+++ main.c
@@ -3,25 +3,26 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 
 /* Generic function for any iterator of element type int */
-static void print_intit(Iterator(Int) it)
+static int sum_intit(Iterator(Int) it)
 {
+    int sum = 0;
     while (1) {
         Maybe(Int) res = it.next(it.self, it.ctx);
         if (is_nothing(res)) {
-            break;
+            return sum;
         }
-        printf("%d ", from_just(res, Int));
+        sum += from_just(res, Int);
     }
-    puts("\n");
 }
 
-static void print_intarr(int** arr, size_t sz)
+static int sum_intarr(int** arr, size_t sz)
 {
     ArrItCtx ctx      = {.size = sz};
     IntIterator intit = prep_intarr_itr(arr, &ctx);
-    print_intit(intit);
+    return sum_intit(intit);
 }
 
 int main(void)
@@ -29,7 +30,8 @@ int main(void)
     /* Use array's iterator instance */
     int arr[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
     int* parr = arr;
-    print_intarr(&parr, sizeof(arr) / sizeof(*arr));
+    int sumarr = sum_intarr(&parr, sizeof(arr) / sizeof(*arr));
+    printf("Sum of array values: %d\n", sumarr);
 
     return 0;
 }
