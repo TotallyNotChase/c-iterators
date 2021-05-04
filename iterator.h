26fc71cd852066cfe6229f892b8def38380e2054
Chase <44284917+TotallyNotChase@users.noreply.github.com>
2021-05-04 14:23:33 +0630

    Add documentation

--- iterator.h
+++ iterator.h
@@ -4,11 +4,38 @@
 #include "maybe.h"
 #include "typeclass.h"
 
+/*
+Convenience macro to declare an Iterator typeclass of a specific element type
+
+@param ElmntTypename - The "canonical" type name of the type this iterator will yield, this is purely subjective and
+upto the user to decide, however the type names for each type **must** be consistent. A Maybe(ElmntTypename) for the
+given ElmntTypename **must** also exist
+*/
 #define DeclareIteratorOf(ElmntTypename)                                                                               \
     typeclass(Maybe(ElmntTypename) (*next)(void* self, void* ctx); void* ctx) ElmntTypename##Iterator
 
+/*
+Convenience macro to get the name of the Iterator with given element type name
+
+@param ElmntTypename - The "canonical" type name of a type, must be the same type name used while declaring the iterator
+typeclass
+*/
 #define Iterator(ElmntTypename) ElmntTypename##Iterator
 
+/*
+Implement a wrapper function for a specific type, which takes in a value of that type and wraps it in an `Iterator`
+instance - essentially making the type a generic "Iterable" - which can be passed around to functions that take in a
+generic `Iterable`.
+
+Note that the term "generic" is used here in the context of the **input**.
+As in, the function taking a generic iterable, does not care about what type the iterable was generated from; but, does
+care about what element type the iterator yields
+
+@param ItrType - The concrete (semantic) type this impl is for - e.g `int*` if the impl is for an array of
+ints
+@param ElmntTypename - The "canonical" type name of the element of the type this impl is for - e.g type name of `int` if
+the impl is for an array of ints
+*/
 #define impl_iterator(ItrType, ElmntTypename, CtxType, next_f, Name)                                                   \
     ElmntTypename##Iterator Name(ItrType* x, CtxType* init_ctx)                                                        \
     {                                                                                                                  \
--- typeclass.h
+++ typeclass.h
@@ -1,6 +1,12 @@
 #ifndef IT_TYPECLASS_H
 #define IT_TYPECLASS_H
 
+/*
+Defines a typeclass with the given members/functions and the self argument
+
+@param membs - A semicolon separated list of typeclass members.
+These are usually functions that take a `void* self` (or more arguments)
+*/
 #define typeclass(membs)                                                                                               \
     typedef struct                                                                                                     \
     {                                                                                                                  \
