a0020e79a2efc9dc7f302eb467dca57538046e15
Chase <44284917+TotallyNotChase@users.noreply.github.com>
2021-05-09 20:30:00 +0630

    Use `take_u32it` function

--- examples/fibbonacci.c
+++ examples/fibbonacci.c
@@ -1,16 +1,20 @@
 #include "examples.h"
 #include "fibonacci_iterable.h"
+#include "iterable_utils.h"
 
 #include <inttypes.h>
 #include <stdlib.h>
 
 void test_fibonacci(void)
 {
-    Fibonacci fib    = {.curr = 0, .next = 1};
-    Iterable(U32) it = prep_fib_itr(&fib);
+    Fibonacci fib      = {.curr = 0, .next = 1};
+    Iterable(U32) it   = prep_fib_itr(&fib); /* Infinite fibonacci sequence iterable */
+    Iterable(U32) it10 = take_u32it(it, 10); /* Iterable of the first 10 items in the sequence */
     /* Print the first 10 items */
-    for (size_t i = 0; i < 10; i++) {
-        printf("%" PRIu32 " ", from_just_(it.tc.next(it.self)));
+    foreach (U32, res, it10) {
+        printf("%" PRIu32 " ", from_just_(res));
     }
+    /* Free the self argument of the iterable returned by `take_u32it` */
+    free(it10.self);
     puts("");
 }
