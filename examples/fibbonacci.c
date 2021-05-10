06eae908750d62fcc530f645dd656ee985c00b91
Chase <44284917+TotallyNotChase@users.noreply.github.com>
2021-05-10 19:33:36 +0700

    Demonstrate taking from partially consumed iterator

--- examples/fibbonacci.c
+++ examples/fibbonacci.c
@@ -14,4 +14,11 @@ void test_fibonacci(void)
         printf("%" PRIu32 " ", n);
     }
     puts("");
+    /* `it10` has consumed 10 items from `it` - take the next 10 items */
+    Iterable(uint32_t) it10_10 = take_from(it, 10, uint32_t); /* Iterable of the next 10 items from the sequence */
+    /* Print the next 10 items */
+    foreach (uint32_t, n, it10_10) {
+        printf("%" PRIu32 " ", n);
+    }
+    puts("");
 }
